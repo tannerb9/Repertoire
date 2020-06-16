ec86ec237e852e8f524611e950a598ff8febe1c5
tannerb9 <tanner-brainard@hotmail.com>
2020-06-16 11:52:26 -0500

    notes display; remove Notes <h3> once current tab is indicated to user

--- src/components/tests/TestNotes.js
+++ src/components/tests/TestNotes.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DataManager from "../../modules/DataManager";
 
-const TestNotes = () => {
-  return null;
+const TestNotes = (props) => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    DataManager.getWithObjs("recipes", props.match.params.testId, "notes").then(
+      (test) => {
+        setNotes(test.notes);
+      }
+    );
+  }, [props.match.params.testId]);
+
+  // REMOVE H3 NOTES ONCE TAB IS INDICATED VIA UNDERLINE ETC
+  return (
+    <>
+      <div>
+        <h3>Notes</h3>
+      </div>
+      <div className="container-notes">
+        {notes.map((note) => (
+          <p key={note.id}>{note.info}</p>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default TestNotes;
