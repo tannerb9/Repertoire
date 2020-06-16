0731ba07dd4e3390b20c758332f2bb40b56cb191
tannerb9 <tanner-brainard@hotmail.com>
2020-06-16 11:13:56 -0500

    fullCards now display respective navbars

--- src/components/navbars/TestNavBar.js
+++ src/components/navbars/TestNavBar.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const RecipeNavBar = (props) => {
-  const recipeId = props.match.params.recipeId;
+const TestNavBar = (props) => {
+  const testId = props.match.params.testId;
+
   return (
     <nav className="recipeNavBar">
       <ul className="container">
         <li>
-          <Link className="nav-link" to={`/recipe/${recipeId}`}>
+          <Link className="nav-link" to={`/test/${testId}`}>
             Overview
           </Link>
         </li>
         <li>
-          <Link className="nav-link" to={`/recipe/${recipeId}/notes`}>
+          <Link className="nav-link" to={`/test/${testId}/notes`}>
             Notes
           </Link>
         </li>
         <li>
-          <Link className="nav-link" to={`/recipe/${recipeId}/directions`}>
+          <Link className="nav-link" to={`/test/${testId}/directions`}>
             Directions
           </Link>
         </li>
@@ -26,4 +27,4 @@ const RecipeNavBar = (props) => {
   );
 };
 
-export default RecipeNavBar;
+export default TestNavBar;
---
+++ src/components/recipes/RecipeNotes.js
@@ -0,0 +1,8 @@
+import React from "react";
+import DataManager from "../../modules/DataManager";
+
+const RecipeNotes = () => {
+  return null;
+};
+
+export default RecipeNotes;
