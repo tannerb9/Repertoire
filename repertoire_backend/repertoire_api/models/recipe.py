66da729d2237f6ed1fd5a2fcc40303afaf91313d
Tanner Brainard <tannerbrainard9@gmail.com>
2024-06-18 18:02:07 -0400

    update: added Meta class and __str__ to Recipe model

--- repertoire_backend/repertoire_api/models/recipe.py
+++ repertoire_backend/repertoire_api/models/recipe.py
@@ -7,3 +7,10 @@ class Recipe(models.Model):
     cook_time = models.IntegerField()
     is_test = models.BooleanField()
     original_recipe_id = models.ForeignKey('self')
+    
+    class Meta:
+        verbose_name = ("Recipe")
+        verbose_name_plural = ("Recipes")
+        
+    def __str__(self):
+        return self.title
